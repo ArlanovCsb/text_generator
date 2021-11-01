3af4409acea1f74dcd725e16732914e8519f46a3
Александр Арланов <arlanov.csb@yandex.ru>
2021-11-01 09:03:04 +0300

    Stage 3

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from collections import defaultdict, Counter
 from pathlib import Path
 from typing import List
 
@@ -16,18 +17,22 @@ def main():
     with open(file_name, 'r', encoding='utf-8') as f:
         tokens = regexp_tokenize(f.read(), r'\S+')
     bigrams = get_bigrams(tokens)
-    print(f'Number of bigrams: {len(bigrams)}')
+    m_chain = defaultdict(Counter)
+    for h, t in bigrams:
+        m_chain[h].update([t])
+    # print(f'Number of bigrams: {len(bigrams)}')
+    m_chain = {k: v.most_common() for k, v in m_chain.items()}
     while True:
         idx = input()
         if idx == 'exit':
             break
+        print(f'Head: {idx}')
         try:
-            print(f'Head: {bigrams[int(idx)][0]}\tTail: {bigrams[int(idx)][1]}')
-        except ValueError:
-            print('Type Error. Please input an integer.')
-            continue
-        except IndexError:
-            print('Index Error. Please input a value that is not greater than the number of all bigrams.')
+            tails = m_chain[idx]
+            for k, v in tails:
+                print(f'Tail: {k}\tCount: {v}')
+        except KeyError:
+            print('Key Error. The requested word is not in the model. Please input another word.')
             continue
 
 
