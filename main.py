eaf78c1a156ceb62c64e1121e5d54f47df9b37fd
Александр Арланов <arlanov.csb@yandex.ru>
2021-11-04 08:30:03 +0300

    Stage 3

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 from collections import defaultdict, Counter
 from pathlib import Path
-from typing import List
+import random
+from typing import List, Dict
 
 from nltk.tokenize import regexp_tokenize
 
 DATA_DIR_NAME = 'data'
+NUM_WORDS = 10
+NUM_SENTENCES = 10
+RANDOM_SEED = 23
+random.seed(RANDOM_SEED)
 
 
 def get_bigrams(corpus: List) -> List:
     return [(corpus[i], corpus[i + 1]) for i in range(len(corpus) - 1)]
+
+
+def get_markov_chain(bigrams: List) -> Dict:
+    m_chain = defaultdict(Counter)
+    for h, t in bigrams:
+        m_chain[h].update([t])
+    return {k: v.most_common() for k, v in m_chain.items()}
+
+
+def get_word_from_chain(tails: List) -> str:
+    return random.choices([x[0] for x in tails], [x[1] for x in tails])[0]
 
 
 def main():
@@ -17,23 +33,16 @@ def main():
     with open(file_name, 'r', encoding='utf-8') as f:
         tokens = regexp_tokenize(f.read(), r'\S+')
     bigrams = get_bigrams(tokens)
-    m_chain = defaultdict(Counter)
-    for h, t in bigrams:
-        m_chain[h].update([t])
-    # print(f'Number of bigrams: {len(bigrams)}')
-    m_chain = {k: v.most_common() for k, v in m_chain.items()}
-    while True:
-        idx = input()
-        if idx == 'exit':
-            break
-        print(f'Head: {idx}')
-        try:
-            tails = m_chain[idx]
-            for k, v in tails:
-                print(f'Tail: {k}\tCount: {v}')
-        except KeyError:
-            print('Key Error. The requested word is not in the model. Please input another word.')
-            continue
+    m_chain = get_markov_chain(bigrams)
+    head = random.choice(list(m_chain.keys()))
+    sentence = [head]
+    for _ in range(NUM_WORDS * NUM_SENTENCES - 1):
+        word = get_word_from_chain(m_chain[head])
+        sentence.append(word)
+        head = word
+        if len(sentence) == 10:
+            print(' '.join(sentence))
+            sentence.clear()
 
 
 if __name__ == '__main__':
